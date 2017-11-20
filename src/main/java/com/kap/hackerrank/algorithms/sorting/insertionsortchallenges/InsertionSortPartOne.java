4f9c6d2619afe2e4cf5bce4f154e70adc2db5db5
aftkantoniou <kantoniou@appliedfinancialtechnology.com>
2017-11-20 14:52:32 +0000

    optimal implementation of insertion sort

--- src/main/java/com/kap/hackerrank/algorithms/sorting/insertionsortchallenges/InsertionSortPartOne.java
+++ src/main/java/com/kap/hackerrank/algorithms/sorting/insertionsortchallenges/InsertionSortPartOne.java
@@ -8,33 +8,26 @@ public class InsertionSortPartOne {
         Scanner in = new Scanner(System.in);
         int s = in.nextInt();
         int[] ar = new int[s];
-        for(int i=0;i<s;i++){
-            ar[i]=in.nextInt();
+        for (int i = 0; i < s; i++) {
+            ar[i] = in.nextInt();
         }
         insertIntoSorted(ar);
     }
 
     private static void insertIntoSorted(int[] ar) {
         int temp = ar[ar.length - 1];
-        for(int i = ar.length - 1; i > 0; i--) {
-            if (temp < ar[i-1]) {
-                ar[i] = ar[i-1];
-            } else {
-                ar[i] = temp;
-                break;
-            }
+        int i;
+        for (i = ar.length - 1; (i > 0) && (temp < ar[i - 1]); i--) {
+            ar[i] = ar[i - 1];
             printArray(ar);
         }
-
-        if(temp < ar[0]) {
-            ar[0] = temp;
-        }
+        ar[i] = temp;
         printArray(ar);
     }
 
     private static void printArray(int[] ar) {
-        for(int n: ar){
-            System.out.print(n+" ");
+        for (int n : ar) {
+            System.out.print(n + " ");
         }
         System.out.println("");
     }
