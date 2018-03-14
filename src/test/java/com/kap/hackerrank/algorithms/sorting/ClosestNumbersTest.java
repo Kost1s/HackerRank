88f6488562ad3bee93b05435001008abd065fab8
aftkantoniou <kantoniou@appliedfinancialtechnology.com>
2018-03-14 09:00:57 +0000

    Problem: Closest Numbers
    - test methods renaming

--- src/test/java/com/kap/hackerrank/algorithms/sorting/ClosestNumbersTest.java
+++ src/test/java/com/kap/hackerrank/algorithms/sorting/ClosestNumbersTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class ClosestNumbersTest {
 
     @Test
-    public void letSee() {
+    public void sameMinDiff() {
         int[] ar = {5, 4, 3, 2};
         int[] test = ClosestNumbers.closestNumbers(ar);
         for(int e : test){
@@ -17,7 +17,7 @@ public class ClosestNumbersTest {
     }
 
     @Test
-    public void letSeeTwo() {
+    public void uniqueMinDiff() {
         int[] ar = {-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854};
         int[] test = ClosestNumbers.closestNumbers(ar);
         for(int e : test){
@@ -26,7 +26,7 @@ public class ClosestNumbersTest {
     }
 
     @Test
-    public void letSeeThree() {
+    public void multiplePairsSameMinDiff() {
         int[] ar = {-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854, -520, -470};
         int[] test = ClosestNumbers.closestNumbers(ar);
         for(int e : test){
