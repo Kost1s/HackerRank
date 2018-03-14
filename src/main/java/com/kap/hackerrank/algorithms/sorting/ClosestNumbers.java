12ed7a1a691cb246f9ef96ee2aadf841872af3a6
Kost1s <antonioucostis@gmail.com>
2018-03-14 00:41:16 +0000

    smaller solution

--- src/main/java/com/kap/hackerrank/algorithms/sorting/ClosestNumbers.java
+++ src/main/java/com/kap/hackerrank/algorithms/sorting/ClosestNumbers.java
@@ -25,38 +25,29 @@ public class ClosestNumbers {
     }
 
     private static int[] mapDifferences(int[] ar) {
-        int i = 0;
-        Map<Integer, List<Integer>> absDifferences = new HashMap<>();
-
-        while (i < (ar.length - 1)) {
-            Integer diff = Math.abs(ar[i] - ar[i + 1]);
-            absDifferences.put(diff, addPairs(absDifferences, diff, ar, i));
-            i++;
-        }
-
-        return diffPairs(absDifferences);
-    }
-
-    private static List<Integer> addPairs(Map<Integer, List<Integer>> absDifferences, Integer diff, int[] ar, int i) {
+        int j = 0;
+        int prevDiff = Math.abs(ar[j] - ar[j + 1]);
         List<Integer> arrayPairs = new LinkedList<>();
 
-        if(absDifferences.containsKey(diff)) {
-            arrayPairs = absDifferences.get(diff);
-            arrayPairs.add(ar[i]);
-            arrayPairs.add(ar[i + 1]);
-        } else {
-            arrayPairs.clear();
-            arrayPairs.add(ar[i]);
-            arrayPairs.add(ar[i + 1]);
+        int newDiff;
+        while (j < (ar.length - 1)) {
+            newDiff = Math.abs(ar[j] - ar[j + 1]);
+            if(newDiff == prevDiff) {
+                arrayPairs.add(ar[j]);
+                arrayPairs.add(ar[j + 1]);
+                prevDiff = newDiff;
+            }
+
+            if(newDiff < prevDiff) {
+                arrayPairs.clear();
+                arrayPairs.add(ar[j]);
+                arrayPairs.add(ar[j + 1]);
+                prevDiff = newDiff;
+            }
+
+            j++;
         }
 
-        return arrayPairs;
+        return arrayPairs.stream().mapToInt(i-> i).toArray();
     }
-
-    private static int[] diffPairs(Map<Integer, List<Integer>> diffMap) {
-        int minDiff = Collections.min(diffMap.keySet());
-        List<Integer> test = diffMap.get(minDiff);
-        return test.stream().mapToInt(i->i).toArray();
-    }
-
 }
