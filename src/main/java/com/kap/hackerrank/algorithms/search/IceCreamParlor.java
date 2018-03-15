7f34b81cc56198abca9a4423bacd19fa18f04b10
aftkantoniou <kantoniou@appliedfinancialtechnology.com>
2018-03-15 18:01:07 +0000

    Problem: Ice Cream Parlor
    - this alternative solution now passes all tests also

--- src/main/java/com/kap/hackerrank/algorithms/search/IceCreamParlor.java
+++ src/main/java/com/kap/hackerrank/algorithms/search/IceCreamParlor.java
@@ -13,12 +13,12 @@ public class IceCreamParlor {
         Scanner in = new Scanner(System.in);
 
         int trips = in.nextInt();
-        for(int t = 0; t < trips; t++){
+        for (int t = 0; t < trips; t++) {
             int moneyPooled = in.nextInt();
             int numberOfFlavors = in.nextInt();
 
             int[] inputAr = new int[numberOfFlavors];
-            for(int i = 0; i < numberOfFlavors; i++){
+            for (int i = 0; i < numberOfFlavors; i++) {
                 inputAr[i] = in.nextInt();
             }
 
@@ -27,8 +27,6 @@ public class IceCreamParlor {
                 System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
             }
             System.out.println("");
-
-
         }
         in.close();
     }
@@ -36,35 +34,33 @@ public class IceCreamParlor {
     public static int[] getFlavorNumbers(int moneyPooled, int[] flavors) {
         Map<Integer, List<Integer>> flavorsMap;
         int[] flavorsCosts;
-        int[] flavorNumbers =  new int[2];
-
+        int[] flavorsNumbers = new int[2];
 
         flavorsMap = getFlavorsMap(flavors);
         flavorsCosts = getFlavorCosts(moneyPooled, flavors);
 
-        if(flavorsCosts[0] != flavorsCosts[1]) {
-            flavorNumbers[0] = flavorsMap.get(flavorsCosts[0]).get(0);
-            flavorNumbers[1] = flavorsMap.get(flavorsCosts[1]).get(0);
+        if (flavorsCosts[0] != flavorsCosts[1]) {
+            flavorsNumbers[0] = flavorsMap.get(flavorsCosts[0]).get(0);
+            flavorsNumbers[1] = flavorsMap.get(flavorsCosts[1]).get(0);
         } else {
-            flavorNumbers[0] = flavorsMap.get(flavorsCosts[0]).get(0);
-            flavorNumbers[1] = flavorsMap.get(flavorsCosts[1]).get(1);
+            flavorsNumbers[0] = flavorsMap.get(flavorsCosts[0]).get(0);
+            flavorsNumbers[1] = flavorsMap.get(flavorsCosts[1]).get(1);
         }
-        return flavorNumbers;
-    }
 
+        Arrays.sort(flavorsNumbers);
+        return flavorsNumbers;
+    }
 
     private static Map<Integer, List<Integer>> getFlavorsMap(int[] flavors) {
         Map<Integer, List<Integer>> flavorsMap = new HashMap<>();
+        List<Integer> flavorsNumbers;
 
-        for(int i=0; i<flavors.length; i++) {
-            List<Integer> flavorsNumbers = new ArrayList<>();
-
-            if(!flavorsMap.containsKey(flavors[i])) {
-                flavorsNumbers.add(i+1);
-                flavorsMap.put(flavors[i], flavorsNumbers);
+        for (int i = 0; i < flavors.length; i++) {
+            if (!flavorsMap.containsKey(flavors[i])) {
+                flavorsMap.put(flavors[i], new ArrayList<>(Collections.singletonList(i + 1)));
             } else {
                 flavorsNumbers = flavorsMap.get(flavors[i]);
-                flavorsNumbers.add(i+1);
+                flavorsNumbers.add(i + 1);
                 flavorsMap.put(flavors[i], flavorsNumbers);
             }
         }
@@ -75,11 +71,11 @@ public class IceCreamParlor {
         int[] flavorsNumbers = new int[2];
 
         Arrays.sort(flavorsCosts);
-        for(int i=0; i<flavorsCosts.length; i++) {
-            int secondFlavorIndex = binarySearch(flavorsCosts, (moneyPooled-flavorsCosts[i]));
-            if((secondFlavorIndex != -1) && (i != secondFlavorIndex) ) {
-                flavorsNumbers[0] =  flavorsCosts[i];
-                flavorsNumbers[1] =  flavorsCosts[secondFlavorIndex];
+        for (int i = 0; i < flavorsCosts.length; i++) {
+            int secondFlavorIndex = binarySearch(flavorsCosts, (moneyPooled - flavorsCosts[i]));
+            if ((secondFlavorIndex >= 0) && (i != secondFlavorIndex)) {
+                flavorsNumbers[0] = flavorsCosts[i];
+                flavorsNumbers[1] = flavorsCosts[secondFlavorIndex];
                 break;
             }
         }
