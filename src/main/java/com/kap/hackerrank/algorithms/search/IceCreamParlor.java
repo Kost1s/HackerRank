52872a0745638880175736223bd0580cecca55c1
aftkantoniou <kantoniou@appliedfinancialtechnology.com>
2018-03-15 17:33:55 +0000

    Problem: Ice Cream Parlor
    - this solution passes the tests

--- src/main/java/com/kap/hackerrank/algorithms/search/IceCreamParlor.java
+++ src/main/java/com/kap/hackerrank/algorithms/search/IceCreamParlor.java
@@ -35,30 +35,32 @@ public class IceCreamParlor {
 
     public static int[] getFlavorNumbers(int moneyPooled, int[] flavors) {
         Map<Integer, List<Integer>> flavorsMap;
-        int[] flavorsCosts;
-        int[] flavorNumbers =  new int[2];
-
+        int[] flavorsNumbers =  new int[2];
 
         flavorsMap = getFlavorsMap(flavors);
-        flavorsCosts = getFlavorCosts(moneyPooled, flavors);
-
-        if(flavorsCosts[0] != flavorsCosts[1]) {
-            flavorNumbers[0] = flavorsMap.get(flavorsCosts[0]).get(0);
-            flavorNumbers[1] = flavorsMap.get(flavorsCosts[1]).get(0);
-        } else {
-            flavorNumbers[0] = flavorsMap.get(flavorsCosts[0]).get(0);
-            flavorNumbers[1] = flavorsMap.get(flavorsCosts[1]).get(1);
+        Arrays.sort(flavors);
+        for(int i=0; i<flavors.length; i++) {
+            int secondFlavorIndex = binarySearch(flavors, moneyPooled - flavors[i]);
+            if((secondFlavorIndex >= 0) && (flavors[i] != flavors[secondFlavorIndex])) {
+                flavorsNumbers[0] =  flavorsMap.get(flavors[i]).get(0);
+                flavorsNumbers[1] =  flavorsMap.get(flavors[secondFlavorIndex]).get(0);
+                break;
+            } else if ((secondFlavorIndex >= 0) && (flavors[i] == flavors[secondFlavorIndex])) {
+                flavorsNumbers[0] =  flavorsMap.get(flavors[i]).get(0);
+                flavorsNumbers[1] =  flavorsMap.get(flavors[secondFlavorIndex]).get(1);
+                break;
+            }
         }
-        return flavorNumbers;
-    }
 
+        Arrays.sort(flavorsNumbers);
+        return flavorsNumbers;
+    }
 
     private static Map<Integer, List<Integer>> getFlavorsMap(int[] flavors) {
         Map<Integer, List<Integer>> flavorsMap = new HashMap<>();
         List<Integer> flavorsNumbers;
 
         for(int i=0; i<flavors.length; i++) {
-
             if(!flavorsMap.containsKey(flavors[i])) {
                 flavorsMap.put(flavors[i], new ArrayList<>(Collections.singletonList(i+1)));
             } else {
@@ -68,21 +70,6 @@ public class IceCreamParlor {
             }
         }
         return flavorsMap;
-    }
-
-    private static int[] getFlavorCosts(int moneyPooled, int[] flavorsCosts) {
-        int[] flavorsNumbers = new int[2];
-
-        Arrays.sort(flavorsCosts);
-        for(int i=0; i<flavorsCosts.length; i++) {
-            int secondFlavorIndex = binarySearch(flavorsCosts, (moneyPooled-flavorsCosts[i]));
-            if((secondFlavorIndex != -1) && (i != secondFlavorIndex) ) {
-                flavorsNumbers[0] =  flavorsCosts[i];
-                flavorsNumbers[1] =  flavorsCosts[secondFlavorIndex];
-                break;
-            }
-        }
-        return flavorsNumbers;
     }
 
 }
