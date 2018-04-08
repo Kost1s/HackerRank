80054dfa595c2ed37345884c5537a62b0be71105
Kost1s <antonioucostis@gmail.com>
2018-04-08 18:24:03 +0100

    Problem: Java Stack
    - solution passes all tests

--- src/main/java/com/kap/hackerrank/languagespecific/datastructures/CharStack.java
+++ src/main/java/com/kap/hackerrank/languagespecific/datastructures/CharStack.java
@@ -2,6 +2,7 @@ package com.kap.hackerrank.languagespecific.datastructures;
 
 import java.util.Scanner;
 import java.util.Stack;
+
 /**
  * @author Konstantinos Antoniou
  */
@@ -14,69 +15,46 @@ public class CharStack {
     private static final char R_BRACKET = '}';
     private static final char R_BRACE = ']';
 
-
-
-    public static void main(String []argh) {
-        Stack charStack = new Stack();
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            String input = sc.next();
+            String charSequence = sc.next();
 
-            for (int i = 0; i < input.length(); i++) {
-                if (input.charAt(i) == L_PAREN) {
-                    charStack.push(input.charAt(i));
-                } else if (input.charAt(i) == L_BRACKET) {
-                    charStack.push(input.charAt(i));
-                } else if (input.charAt(i) == L_BRACE) {
-                    charStack.push(input.charAt(i));
-                }
-
-                if (input.charAt(i) == R_PAREN) {
-                    if (charStack.isEmpty()) {
-                        System.out.println("false");
-                        break;
-                    }
-
-                    if (!(charStack.pop().equals(L_PAREN))) {
-                        System.out.println("false");
-                        break;
-                    }
-                }
-
-                if (input.charAt(i) == R_BRACKET) {
-                    if (charStack.isEmpty()) {
-                        System.out.println("false");
-                        break;
-                    }
-
-                    if (!(charStack.pop().equals(L_BRACKET))) {
-                        System.out.println("false");
-                        break;
-                    }
-                }
-
-                if (input.charAt(i) == R_BRACE) {
-                    if (charStack.isEmpty()) {
-                        System.out.println("false");
-                        break;
-                    }
-
-                    if (!(charStack.pop().equals(L_BRACE))) {
-                        System.out.println("false");
-                        break;
-                    }
-                }
-
-
-
+            Boolean isSequenceValid = false;
+            if ((charSequence.length() % 2) == 0) {
+                isSequenceValid = checkIfValid(charSequence);
             }
-
-            System.out.println("true");
-
+            System.out.println(isSequenceValid);
         }
-
     }
 
+    public static Boolean checkIfValid(String charSequence) {
+        Stack<Character> charStack = new Stack();
+
+        for (int i = 0; i < charSequence.length(); i++) {
+            if ((charSequence.charAt(i) == L_PAREN) || (charSequence.charAt(i) == L_BRACE) ||
+                    (charSequence.charAt(i) == L_BRACKET)) {
+                charStack.push(charSequence.charAt(i));
+            }
+
+            if (!charStack.empty()) {
+                if ((charSequence.charAt(i) == R_PAREN) && !charStack.pop().equals(L_PAREN)) {
+                    return false;
+                }
+
+                if ((charSequence.charAt(i) == R_BRACE) && !charStack.pop().equals(L_BRACE)) {
+                    return false;
+                }
+
+                if ((charSequence.charAt(i) == R_BRACKET) && !charStack.pop().equals(L_BRACKET)) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
---
+++ src/test/java/com/kap/hackerrank/languagespecific/datastructures/CharStackTest.java
@@ -0,0 +1,34 @@
+package com.kap.hackerrank.languagespecific.datastructures;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class CharStackTest {
+
+    @Test
+    public void evenSequenceIsTrueOne() {
+        String charSequence = "{}()";
+        assertTrue(CharStack.checkIfValid(charSequence));
+    }
+
+    @Test
+    public void evenSequenceIsTrueTwo() {
+        String charSequence = "({()})";
+        assertTrue(CharStack.checkIfValid(charSequence));
+    }
+
+    @Test
+    public void evenSequenceIsTrueThree() {
+        String charSequence = "[]";
+        assertTrue(CharStack.checkIfValid(charSequence));
+    }
+
+    @Test
+    public void oddSequenceIsFalse() {
+        String charSequence = "{}(";
+        assertTrue(CharStack.checkIfValid(charSequence));
+    }
+
+
+}
