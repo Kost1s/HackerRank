0145ed4fe099895806cc5e31100420ed1141e8b7
Kost1s <antonioucostis@gmail.com>
2018-05-07 23:26:19 +0100

    Problem: Equal Stacks
    - solution in progress

--- src/main/java/com/kap/hackerrank/datastructures/stacks/EqualStacks.java
+++ src/main/java/com/kap/hackerrank/datastructures/stacks/EqualStacks.java
@@ -14,18 +14,33 @@ public class EqualStacks {
             1 1 4 1
 
 
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         Stack<StackNode> stackOne = new Stack<>();
+        Stack<StackNode> stackTwo = new Stack<>();
+        Stack<StackNode> stackThree = new Stack<>();
 
         int stacksNo = 3;
         int stackOneCapacity = sc.nextInt();
         int stackTwoCapacity = sc.nextInt();
         int stackThreeCapacity = sc.nextInt();
 
-        for(int i=0; i < stackOneCapacity; i++) {
+        int currentSum = Integer.MIN_VALUE;
+        int stackValue;
+
+        for (int i = 0; i < stackOneCapacity; i++) {
+            stackValue = sc.nextInt();
+            stackOne.push(new StackNode(stackValue, (currentSum + stackValue)));
+        }
+
+        currentSum = Integer.MIN_VALUE;
+        for (int i = 0; i < stackTwoCapacity; i++) {
+            stackValue = sc.nextInt();
+            stackTwo.push(new StackNode(stackValue, (currentSum + stackValue)));
+        }
+
+        for(int i =0; i< stackThreeCapacity; i++) {
 
         }
     }
@@ -40,6 +55,5 @@ public class EqualStacks {
             this.currentSum = currentSum;
         }
     }
-
 
 }
