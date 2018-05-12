98127e0f5d5cb69a5aee2a4d8aa5dc77e0ee03db
Kost1s <antonioucostis@gmail.com>
2018-05-12 12:46:09 +0100

    Problem: Equal Stacks
    - solution in progress

--- src/main/java/com/kap/hackerrank/datastructures/stacks/EqualStacks.java
+++ src/main/java/com/kap/hackerrank/datastructures/stacks/EqualStacks.java
@@ -12,7 +12,8 @@ public class EqualStacks {
 
     public static void main(String[] args) {
         List<Stack<StackNode>> stacks = readStacks();
-
+        int height = findHeight(stacks, 0);
+        System.out.println(height);
     }
 
     private static List<Stack<StackNode>> readStacks() {
@@ -25,14 +26,47 @@ public class EqualStacks {
             Stack<StackNode> stack = new Stack<>();
             String[] stackElements = sc.nextLine().split(" ");
             int currentSum = 0;
-            for (int j = stackElements.length-1; j >= 0; j--) {
-                stackValue = Integer.parseInt(stackElements[j]);
-                currentSum += stackValue;
-                stack.push(new StackNode(stackValue, currentSum));
+            if (stackElements.length != 0) {
+                for (int j = stackElements.length - 1; j >= 0; j--) {
+                    stackValue = Integer.parseInt(stackElements[j]);
+                    currentSum += stackValue;
+                    stack.push(new StackNode(stackValue, currentSum));
+                }
             }
             stacks.add(stack);
         }
         return stacks;
+    }
+
+    private static int findHeight(List<Stack<StackNode>> stacks, int i) {
+        if (checkEmpty(stacks)) {
+            return 0;
+        } else if (checkEqualHeight(stacks)) {
+            return stacks.get(0).peek().currentSum;
+        }
+
+        if()
+        if (i == 3) {
+            i = 0;
+        } else {
+            i++;
+        }
+        stacks.get(i).pop();
+        return findHeight(stacks, i);
+    }
+
+    private static boolean checkEqualHeight(List<Stack<StackNode>> stacks) {
+        return (stacks.get(0).peek().currentSum == stacks.get(1).peek().currentSum) &&
+                (stacks.get(1).peek().currentSum == stacks.get(2).peek().currentSum);
+    }
+
+    private static boolean checkEmpty(List<Stack<StackNode>> stacks) {
+        for (Stack<StackNode> stack : stacks) {
+            if (stack.isEmpty()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static class StackNode {
