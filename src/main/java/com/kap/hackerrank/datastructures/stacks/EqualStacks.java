98d81ba3e9791cd0e2a8dae8b7131e29429a3d63
Kost1s <antonioucostis@gmail.com>
2018-05-12 11:10:29 +0100

    Problem: Equal Stacks
    - solution in progress

--- src/main/java/com/kap/hackerrank/datastructures/stacks/EqualStacks.java
+++ src/main/java/com/kap/hackerrank/datastructures/stacks/EqualStacks.java
@@ -11,24 +11,28 @@ import java.util.Stack;
 public class EqualStacks {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        List<Stack<StackNode>> stacks = readStacks();
 
+    }
+
+    private static List<Stack<StackNode>> readStacks() {
+        Scanner sc = new Scanner(System.in);
         List<Stack<StackNode>> stacks = new ArrayList<>();
 
-        int[] stacksCapacity = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
-
+        sc.nextLine();
         int stackValue;
-
-        for (int stackCapacity : stacksCapacity) {
+        for (int i = 0; i < 3; i++) {
             Stack<StackNode> stack = new Stack<>();
+            String[] stackElements = sc.nextLine().split(" ");
             int currentSum = 0;
-            for (int i = 0; i < stackCapacity; i++) {
-                stackValue = sc.nextInt();
+            for (int j = stackElements.length-1; j >= 0; j--) {
+                stackValue = Integer.parseInt(stackElements[j]);
                 currentSum += stackValue;
                 stack.push(new StackNode(stackValue, currentSum));
             }
             stacks.add(stack);
         }
+        return stacks;
     }
 
     private static class StackNode {
