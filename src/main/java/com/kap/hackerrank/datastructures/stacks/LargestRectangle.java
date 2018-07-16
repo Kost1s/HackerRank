4b51cd4606e0c3b6f3475b17d48c8341b12430ee
aftkantoniou <kantoniou@appliedfinancialtechnology.com>
2018-07-16 09:08:38 +0100

    Problem: Stack - Largest Rectangle
    - solution passes all tests

--- src/main/java/com/kap/hackerrank/datastructures/stacks/LargestRectangle.java
+++ src/main/java/com/kap/hackerrank/datastructures/stacks/LargestRectangle.java
@@ -11,63 +11,48 @@ public class LargestRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int buildingsNo = sc.nextInt();
+        int maxHeight = Integer.MIN_VALUE;
+        int height;
+        int position = 0;
+        int lastPosition = 0;
 
-        int maxArea = Integer.MIN_VALUE;
-        int minPushHeight = Integer.MAX_VALUE;
-        int minPopHeight = Integer.MAX_VALUE;
-        int positionPush = 0;
-        int positionPop = 0;
+        Stack<Integer> pStack = new Stack<>();
+        Stack<Integer> hStack = new Stack<>();
+
         int currentHeight;
-
-        Stack<Integer> s = new Stack<>();
+        int buildingsNo = sc.nextInt();
         for (int i = 0; i < buildingsNo; i++) {
             currentHeight = sc.nextInt();
 
-            // incoming is bigger than top or stack empty
-            if (s.empty() || (!s.empty() && (currentHeight >= s.peek()))) {
-                s.push(currentHeight);
-                positionPush++;
-                if (currentHeight < minPushHeight) {
-                    minPushHeight = currentHeight;
-                }
-                maxArea = getMaxArea(minPushHeight, positionPush, currentHeight, maxArea);
+            if ((hStack.isEmpty()) || (!hStack.isEmpty() && (currentHeight >= hStack.peek()))) {
+                hStack.push(currentHeight);
+                pStack.push(position);
+                position++;
             }
 
-            // incoming is less than top
-            if(!s.isEmpty() && (currentHeight < s.peek())) {
-                minPopHeight = Integer.MAX_VALUE;
-                while (!s.isEmpty() && (currentHeight < s.peek())) {
-                    if (s.peek() <= minPopHeight) {
-                        minPopHeight = s.peek();
-                        positionPop++;
-                        maxArea = getMaxArea(minPopHeight, positionPop, s.pop(), maxArea);
+            if (!hStack.isEmpty() && (currentHeight < hStack.peek())) {
+                while (!hStack.isEmpty() && (currentHeight < hStack.peek())) {
+                    lastPosition = pStack.pop();
+                    height = hStack.pop() * (position - lastPosition);
+                    if (height > maxHeight) {
+                        maxHeight = height;
                     }
                 }
-                s.push(currentHeight);
+                hStack.push(currentHeight);
+                pStack.push(lastPosition);
+                position++;
+            }
+
+        }
+
+        while(!hStack.isEmpty()) {
+            height = hStack.pop() * (position - pStack.pop());
+            if (height > maxHeight) {
+                maxHeight = height;
             }
         }
 
-        while (!s.isEmpty()) {
-            if (s.peek() <= minPopHeight) {
-                minPopHeight = s.peek();
-                positionPop++;
-                maxArea = getMaxArea(minPopHeight, positionPop, s.pop(), maxArea);
-            }
-        }
-
-        System.out.println(maxArea);
-    }
-
-    private static int getMaxArea(int minHeight, int position, int buildingArea, int maxArea) {
-        int currentOverallArea = minHeight * position;
-        if ((buildingArea >= currentOverallArea) && (buildingArea > maxArea)) {
-            return buildingArea;
-        }
-        if ((currentOverallArea >= buildingArea) && (currentOverallArea > maxArea)) {
-            return currentOverallArea;
-        }
-        return maxArea;
+        System.out.println(maxHeight);
     }
 
 }
