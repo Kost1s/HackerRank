cbd19a22a2d466a1b88e2fbf47000580761e317e
Kost1s <antonioucostis@gmail.com>
2018-08-25 15:28:45 +0100

    solution passes all tests

---
+++ src/main/java/com/kap/hackerrank/datastructures/linkedlists/InsertNodeAtSpecificPosition.java
@@ -0,0 +1,112 @@
+package com.kap.hackerrank.datastructures.linkedlists;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author Konstantinos Antoniou
+ */
+public class InsertNodeAtSpecificPosition {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")))) {
+
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            int data = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int position = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            SinglyLinkedListNode llist_head = insertNodeAtPosition(llist.head, data, position);
+
+            printSinglyLinkedList(llist_head, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        scanner.close();
+    }
+
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+
+        if(position == 0) {
+            node.next = head;
+            return head;
+        }
+
+        int i = 1;
+        SinglyLinkedListNode interimNode = head.next;
+        while(i <= (position - 1)) {
+            if(i == (position - 1)) {
+                node.next = interimNode.next;
+                interimNode.next = node;
+            }
+            interimNode = interimNode.next;
+            i++;
+        }
+        return head;
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter)
+            throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+
+}
