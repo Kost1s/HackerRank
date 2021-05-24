d07d46c29859f84141fee26c5c3256341b334951
Kost1s <antonioucostis@gmail.com>
2021-05-24 21:54:52 +0100

    solution completed

---
+++ src/main/java/com/kap/hackerrank/datastructures/trees/InorderTraversal.java
@@ -0,0 +1,58 @@
+package com.kap.hackerrank.datastructures.trees;
+
+import java.util.Scanner;
+
+/**
+ * @author Konstantinos Antoniou
+ */
+public class InorderTraversal {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        inOrder(root);
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node (int data) {
+            this.data = data;
+        }
+    }
+
+}
