3a8b14b350d149ffcd80d74bc926edca1e84b281
Kost1s <antonioucostis@gmail.com>
2021-05-24 21:04:51 +0100

    solution completed

---
+++ src/main/java/com/kap/hackerrank/datastructures/trees/PostOrderTraversal.java
@@ -0,0 +1,59 @@
+package com.kap.hackerrank.datastructures.trees;
+
+import java.util.Scanner;
+
+/**
+ * @author Konstantinos Antoniou
+ */
+public class PostOrderTraversal {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        postOrder(root);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+}
